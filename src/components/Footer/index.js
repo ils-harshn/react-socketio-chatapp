9d0bb90967bc2c391f5d589aee84c057ea11715a
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-05-27 14:44:16 +0530

    footer added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,40 @@
+import Link from "../Link";
+import styles from "./footer.module.css";
+
 const FooterP1 = () => {
-  return <footer>wow</footer>;
+  const LINKS = [
+    {
+      url: "",
+      label: "About",
+    },
+    {
+      url: "",
+      label: "Terms",
+    },
+    {
+      url: "",
+      label: "Privacy",
+    },
+    {
+      url: "",
+      label: "Contact ChatApp Support",
+    },
+    {
+      url: "",
+      label: "Docs",
+    },
+  ];
+  return (
+    <footer className={styles.Footer}>
+      <ul className={styles.Links}>
+        {LINKS.map((link, index) => (
+          <li key={index} className={styles.Link}>
+            <Link to={link.url}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
 };
 
 export default FooterP1;
