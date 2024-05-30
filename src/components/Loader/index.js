7346ee391c0331d679697abeff676e18b6b1c702
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-05-30 12:14:50 +0530

    made loader for full screen more compact

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styles from "./loader.module.css";
 
 export const Spinner = () => {
@@ -5,9 +6,44 @@ export const Spinner = () => {
 };
 
 export const FullScreenLoader = () => {
+  const [warn, setWarn] = useState("");
+
+  useEffect(() => {
+    const interval1 = setTimeout(() => {
+      setWarn("WAIT");
+    }, 2000);
+
+    const interval2 = setTimeout(() => {
+      setWarn("SERVERWAIT");
+    }, 6000);
+
+    const interval3 = setTimeout(() => {
+      setWarn("RELOAD");
+    }, 12000);
+
+    return () => {
+      clearInterval(interval1);
+      clearInterval(interval2);
+      clearInterval(interval3);
+    };
+  }, []);
+
   return (
     <div className={`${styles.FullScreenLoader}`}>
-      <Spinner />
+      <div className="text-center">
+        <div className="full-screen-jcenter">
+          <Spinner />
+        </div>
+        {warn === "WAIT" ? (
+          <p className="mt-16">Please wait</p>
+        ) : warn === "SERVERWAIT" ? (
+          <p className="mt-16">
+            Please wait. The server is taking a longer time than usual.
+          </p>
+        ) : warn === "RELOAD" ? (
+          <p className="mt-16">Lagata hai server fata.</p>
+        ) : null}
+      </div>
     </div>
   );
 };
