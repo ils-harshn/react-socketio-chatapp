d9c8c95a80699c588fdef682f47ee0eba6cf3eae
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-05-31 16:05:23 +0530

    Connection issues handling

--- src/pages/channel/Layout/index.js
+++ src/pages/channel/Layout/index.js
@@ -1,19 +1,33 @@
 import { useEffect, useState } from "react";
-import { Outlet, useParams } from "react-router-dom";
+import { Navigate, Outlet, useParams } from "react-router-dom";
 import { io } from "socket.io-client";
 import notify from "../../../utils/notify";
 import { FullScreenLoader } from "../../../components/Loader";
 import { useDispatch } from "react-redux";
 import { set_socket } from "../../../store/actions/SocketActions/index.types";
+import WebTokenStorer from "../../../utils/webTokenStorer";
+import ROUTES from "../../../router/ROUTES";
+
+const SOCKET_STATUS = {
+  CONNECTIING: 0,
+  CONNECTED: 1,
+  DISCONNECTED: 2,
+  CONNECT_ERROR: 3,
+};
 
 const ChannelLayout = () => {
   const { channelId } = useParams();
-  const [isConnected, setConnected] = useState(false);
+  const [socketStatus, setSocketStatus] = useState(SOCKET_STATUS.CONNECTIING);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const socket = io(
-      `${process.env.REACT_APP_SOCKET_URI}channel-${channelId}`
+      `${process.env.REACT_APP_SOCKET_URI}channel-${channelId}`,
+      {
+        auth: {
+          token: WebTokenStorer.get(),
+        },
+      }
     );
 
     const handleConnect = () => {
@@ -23,24 +37,40 @@ const ChannelLayout = () => {
           socket,
         })
       );
-      setConnected(true);
+      setSocketStatus(SOCKET_STATUS.CONNECTED);
     };
 
     const handleDisconnect = () => {
-      setConnected(false);
+      setSocketStatus(SOCKET_STATUS.DISCONNECTED);
+    };
+
+    const handleConnectError = () => {
+      setSocketStatus(SOCKET_STATUS.CONNECT_ERROR);
     };
 
     socket.on("connect", handleConnect);
     socket.on("disconnect", handleDisconnect);
+    socket.on("connect_error", handleConnectError);
 
     return () => {
       socket.off("connect", handleConnect);
       socket.off("disconnect", handleDisconnect);
+      socket.off("connect_error", handleConnectError);
       socket.close();
     };
   }, [channelId, dispatch]);
 
-  if (isConnected) return <Outlet />;
+  if (socketStatus === SOCKET_STATUS.CONNECTED) return <Outlet />;
+  if (socketStatus === SOCKET_STATUS.DISCONNECTED) {
+    notify.info("You have disconnected from the server.");
+    return <Navigate to={ROUTES.DASHBOARD} />;
+  }
+
+  if (socketStatus === SOCKET_STATUS.CONNECT_ERROR) {
+    notify.error("Could not connect to that channel.");
+    return <Navigate to={ROUTES.DASHBOARD} />;
+  }
+
   return (
     <FullScreenLoader
       warns={[
--- src/pages/channel/dashboard/index.js
+++ src/pages/channel/dashboard/index.js
@@ -1,7 +1,9 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const ChannelDashboard = () => {
   const { socket } = useSelector((reducers) => reducers.useSocketReducer);
+  useEffect(() => {}, [socket]);
   return <h2>{socket.id}</h2>;
 };
 export default ChannelDashboard;
