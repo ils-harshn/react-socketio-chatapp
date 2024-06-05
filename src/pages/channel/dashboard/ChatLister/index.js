fa29913a7bf6299ba7c5f2342f1d2fb7d9708df7
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-06-05 18:22:26 +0530

    listting spaces

--- src/pages/channel/dashboard/ChatLister/index.js
+++ src/pages/channel/dashboard/ChatLister/index.js
@@ -1,7 +1,37 @@
+import { useSelector } from "react-redux";
 import styles from "./ChatLister.module.css";
+import { useEffect, useState } from "react";
+
+const Spaces = () => {
+  const { socket } = useSelector((reducers) => reducers.useSocketReducer);
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    socket.emit("space:req:list");
+    socket.on("space:list", (data) => {
+      setdata(data);
+    });
+  }, [socket]);
+  return (
+    <div>
+      <div>Channels</div>
+      <div>
+        <ul>
+          {data.map((item) => (
+            <li key={item.space._id}>{item.space.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 const ChatLister = () => {
-  return <div className={styles.ChatLister}></div>;
+  return (
+    <div className={styles.ChatLister}>
+      <Spaces />
+    </div>
+  );
 };
 
 export default ChatLister;
