713e81c4e5a1b49e9582b802b105f8073b9fcff6
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-06-07 14:12:05 +0530

    added search box editable content

--- src/components/Navbars/CNavbar.js
+++ src/components/Navbars/CNavbar.js
@@ -1,17 +1,74 @@
 import { useSelector } from "react-redux";
 import styles from "./CNavbar.module.css";
-import { ExitChannelIcon, SearchIcon } from "../../assests/icons";
+import { CrossIcon, ExitChannelIcon, SearchIcon } from "../../assests/icons";
 import { useNavigate } from "react-router-dom";
 import ROUTES from "../../router/ROUTES";
 import notify from "../../utils/notify";
 import InviteMembersModal from "../Modals/InviteMembersModal";
+import { useEffect, useRef, useState } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
+
+const EditableContent = () => {
+  const [input, setInput] = useState("");
+  const inputRef = useRef();
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleClear = () => {
+    setInput("");
+    inputRef.current?.focus();
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  return (
+    <>
+      <div className={styles.EditableContentContainer}>
+        <div className={styles.EdSearchIcon}>
+          <SearchIcon />
+        </div>
+        <div
+          className={styles.EditableContent}
+          tabIndex={-1}
+          onFocus={() => {}}
+        >
+          <input
+            ref={inputRef}
+            tabIndex={"0"}
+            placeholder="Search (well it's better then scrolling)"
+            className={styles.EditableContentInput}
+            onChange={handleInput}
+            value={input}
+          ></input>
+        </div>
+        <div className={styles.EdCrossIcon} onClick={handleClear}>
+          <CrossIcon />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Search = ({ setOpen }) => {
+  return (
+    <OutsideClickHandler onOutsideClick={() => setOpen(false)}>
+      <div className={styles.Search}>
+        <EditableContent />
+      </div>
+    </OutsideClickHandler>
+  );
+};
 
 const CNavbar = () => {
   const navigate = useNavigate();
-
   const { channel, user } = useSelector(
     (reducers) => reducers.useChannelReducer
   );
+  const [isOpen, setOpen] = useState(true);
 
   return (
     <nav className={styles.CNavbar}>
@@ -26,8 +83,9 @@ const CNavbar = () => {
           }}
         />
       </div>
-      <div className={styles.SearchBoxContainer}>
+      <div className={styles.SearchBoxContainer} onClick={() => setOpen(true)}>
         Search {channel.name} <SearchIcon className={styles.SearchIcon} />
+        {isOpen ? <Search setOpen={setOpen} /> : null}
       </div>
     </nav>
   );
